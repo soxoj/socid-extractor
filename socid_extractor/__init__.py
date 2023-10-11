a3d398ea70e73159348dfdb9902e7a91ce5d7634
Soxoj <soxoj@protonmail.com>
2023-10-11 22:03:39 +0200

    Bump to 0.0.26

--- socid_extractor/__init__.py
+++ socid_extractor/__init__.py
@@ -1,6 +1,6 @@
 from .main import extract, parse, parse_cookies, mutate_url
 
-__version__ = '0.0.25'
+__version__ = '0.0.26'
 __title__ = 'socid_extractor'
 __package__ = 'socid_extractor'
 __author__ = 'soxoj'
